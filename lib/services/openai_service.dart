a7c700a9fab33878ae259112afa994a2563ebb59
recepzgrmh <recepzgrmh@gmail.com>
2025-06-03 15:40:09 +0300

    backend eklendi vector store'a belge gönderilip yorumlatılabiliyor

--- lib/services/openai_service.dart
+++ lib/services/openai_service.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:openai_dart/openai_dart.dart';
 
 class OpenAIService {
